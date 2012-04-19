67c5c1d1642d9f689a6edfd59c9a0a3b5a32f803
shigeru nakajima <shigeru.nakajima@gmail.com>
2012-04-19 09:44:54 +0900

    add Tuple Sample

---
+++ TupleSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TupleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myBag = Tuple.Create("お名前", 42, DateTime.Now);
+
+            //値が取れる
+            Console.WriteLine(myBag.Item1);
+            Console.WriteLine(myBag.Item2);
+            Console.WriteLine(myBag.Item3);
+
+            //型も取れる
+            Console.WriteLine(myBag.Item3.GetType());
+
+            //インスタンス生成パターン
+            var myBag2 = Tuple.Create<string, int, DateTime>("name", 42, DateTime.Now);
+            var myBag3 = new Tuple<string, int, DateTime>("name", 42, DateTime.Now);
+        }
+    }
+}
