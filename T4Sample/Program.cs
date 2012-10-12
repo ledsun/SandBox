d52179646b69cfc68f9676c1f368989ca5495e35
ledsun <shigeru.nakajima@gmail.com>
2012-10-12 09:17:54 +0900

    add T4Sample

---
+++ T4Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var page = new MakeHtmlSampleTemplate();
+            var pageContent = page.TransformText();
+            System.IO.File.WriteAllText("outputPage.html", pageContent);
+        }
+    }
+}
