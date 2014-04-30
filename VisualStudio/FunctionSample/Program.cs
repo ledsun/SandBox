d6256aec98304ba1f503ac20f495cff046f0f22e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2014-04-30 13:24:56 +0900

    第一級関数のサンプルを追加します

---
+++ VisualStudio/FunctionSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    class Program
+    {
+        //デリゲート
+        delegate void SimpleFunctionType();
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!1");
+        }
+
+        static void Main(string[] args)
+        {
+            //デリゲート型の変数に関数を代入
+            SimpleFunctionType func = HelloWorld;
+            func.Invoke();
+
+            //匿名メソッド式
+            func = delegate() { Console.WriteLine("Hello World!2"); };
+            func.Invoke();
+
+            //ラムダ式
+            func = () => Console.WriteLine("Hello World!3");
+            func.Invoke();
+
+            //即時実行
+            new Action(() => Console.WriteLine("Hello World!4")).Invoke();
+        }
+    }
+}
