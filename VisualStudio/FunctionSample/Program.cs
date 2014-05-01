525856d6d89b83a3032adba3e6a0f35ae9d83985
shigeru nakajima <shigeru.nakajima@gmail.com>
2014-05-01 16:16:27 +0900

    いろいろなデリゲートを追加します。

--- VisualStudio/FunctionSample/Program.cs
+++ VisualStudio/FunctionSample/Program.cs
@@ -30,8 +30,24 @@ namespace Function
             func = () => Console.WriteLine("Hello World!3");
             func.Invoke();
 
+            //定義済みデリゲート
+            Action func1 = () => Console.WriteLine("Hello World!4");
+
             //即時実行
-            new Action(() => Console.WriteLine("Hello World!4")).Invoke();
+            new Action(() => Console.WriteLine("Hello World!5")).Invoke();
+
+            //引数あり
+            new Action<string>(message => Console.WriteLine(message)).Invoke("Hello World!6");
+
+            //戻り値あり
+            new Func<string>(() => "Hello World!7").Invoke();
+
+            //Comparisionデリゲート
+            Comparison<int> comp = (x, y) => y - x;
+            Console.WriteLine(comp(1, 2));
+            var list = new[] { 1, 2 }.ToList();
+            list.Sort((x, y) => y - x);
+            list.ForEach((i) => Console.WriteLine(i));
         }
     }
 }
