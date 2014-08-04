1b74cf92e1f39b222da4b246c53066c5b552f78a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2014-08-04 10:00:49 +0900

    接続が上手く言っているか分かりやすくするために、ログをこまめに出します。

--- JavaScript/mqtt/mqtt.js/subscriber.js
+++ JavaScript/mqtt/mqtt.js/subscriber.js
@@ -1,11 +1,12 @@
-var mqtt = require('mqtt');
-var client = mqtt.createClient(61613, {
-  username: 'admin',
-  password: 'password'
-});
-
-client.subscribe('message');
-
-client.on('message', function() {
-  console.log(arguments);
-});
+var mqtt = require('mqtt'),
+  client = mqtt
+  .createClient(61613)
+  .on('connect', function() {
+    console.log('connect OK!');
+  })
+  .subscribe('message', function(err, granted) {
+    console.log(err, granted, 'start subscribe');
+  })
+  .on('message', function() {
+    console.log(arguments);
+  })
