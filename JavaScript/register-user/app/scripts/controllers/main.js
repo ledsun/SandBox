a0ce2e51513bd127a50002d02e1d4bc4966b9906
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-12-01 11:15:15 +0900

    Remove a vulnerable old sample

--- JavaScript/register-user/app/scripts/controllers/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('registerUserApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
